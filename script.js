c1e09bb51c816b51ded74cda59069e3ad7c29cd8
galiphasandugan <galiphasandugan@gmail.com>
2023-06-06 19:11:01 +0300

    commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const wrongLetters = [];
 let selectedWord = getRandomWord();
 
 function getRandomWord() {
-    const words = ["javascript","java","python","css","html"];
+    const words = ["javascrıpt","java","python","css","html"];
     return words[Math.floor(Math.random() * words.length)];
 }
 
@@ -73,7 +73,7 @@ PlayAgainBtn.addEventListener('click', function() {
 });
 
 window.addEventListener('keydown', function(e) {
-    if (e.keyCode >= 65 && e.keyCode <= 90 && e.keyCode == 222 )    {        
+    if (e.keyCode >= 65 && e.keyCode <= 90 )    {        
         const letter = e.key;
 
         if (selectedWord.includes(letter)) {
